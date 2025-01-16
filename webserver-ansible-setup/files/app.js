1b3b8eb2288557e393ea60cfb3a699b63e3d6b1e
Damilare Akintola <akintola573@gmail.com>
2025-01-16 11:44:22 +0100

    made changes

---
+++ webserver-ansible-setup/files/app.js
@@ -0,0 +1,7 @@
+// Burger menu toggle for smaller screens (if needed in the future)
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+navToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
